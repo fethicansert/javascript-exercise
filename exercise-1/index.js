4d566bcf7d30e7f0c31d19df46d46f7178c5f898
fethicansert <fethisert0@gmail/com>
2023-07-24 12:11:47 +0300

    javaScript day one

---
+++ exercise-1/index.js
@@ -0,0 +1,9 @@
+let first_name="Fethi"
+let surname="Sert";
+let age = 24;
+let rank = "Muh Atgm"
+
+document.getElementById("name").innerHTML= first_name;
+document.getElementById("surname").innerHTML = surname;
+document.getElementById("age").innerHTML = age;
+document.getElementById("rank").innerHTML = rank;
