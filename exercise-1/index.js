cadc34bcd860914bcd56375e5888b6bfb466ffc4
fethicansert <fethisert0@gmail/com>
2023-07-24 14:21:36 +0300

    Conversations

--- exercise-1/index.js
+++ exercise-1/index.js
@@ -1,9 +1,15 @@
 // let username = window.prompt("What is your name?");
 // console.log(username);
-
-
-let username;
-document.getElementById("myButton").onclick = function(){
-    username = document.getElementById("mytext").value;
-    document.getElementById("mylabel").innerHTML = username;
+let age;
+document.getElementById("button").onclick = function(){
+    age = window.prompt("How old are you?");
+    document.getElementById("age").innerHTML = age;
+    document.getElementById("button-increase").onclick = function(){
+        console.log(typeof age);
+        age = Number(age);
+        console.log(typeof age);
+        age += 1; 
+        document.getElementById("age").innerHTML = age;
+        
+    }
 }
