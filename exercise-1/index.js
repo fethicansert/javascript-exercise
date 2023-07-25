5cdf69d357772a11fe4f33f8843cbb0da7686b9f
fethicansert <fethisert0@gmail/com>
2023-07-25 14:04:40 +0300

    Odev bir yapildi devam edilecek

--- exercise-1/index.js
+++ exercise-1/index.js
@@ -1,10 +1,35 @@
-let pi = 3.14159;
-let radius;
-let circumtance;
+let name;
+let surname;
+let age;
+let rank;
 
-document.getElementById("button").onclick = function(){
-    radius = window.prompt("Enter the radius");
-    radius = Number(radius);
-    circumtance= Math.round(radius*pi*2);
-    document.getElementById("result").innerHTML=circumtance; 
+document.getElementById("submit").onclick =async function(){
+    
+    name = document.getElementById("text-name").value;
+    surname = document.getElementById("text-surname").value;
+    age = document.getElementById("text-age").value;
+    rank = document.getElementById("text-rank").value;
+    console.log(name,surname,age,rank);
+    
+    
+
+   await sleep(1000);
+
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("surname").innerHTML = surname;
+    document.getElementById("age").innerHTML =age;
+    document.getElementById("rank").innerHTML = rank;
+}
+
+document.getElementById("clear").onclick = function(){
+    
+    document.getElementById("text-name").value = "";
+    document.getElementById("text-surname").value = "";
+    document.getElementById("text-age").value = "";
+    document.getElementById("text-rank").value = "";
+ 
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms));
 }
